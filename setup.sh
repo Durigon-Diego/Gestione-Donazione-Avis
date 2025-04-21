9298e75d120ab2ab23b4be454e89f407e2c02900
Diego Durigon <durigon.diego@gmail.com>
2025-04-21 12:06:31 +0200

    Hook fixed

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@
 # Ensure hooks directory exists
 mkdir -p .git/hooks
 
-# Copy the prepare-commit-msg hook
-cp .git-hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
-chmod +x .git/hooks/prepare-commit-msg
+# link the prepare-commit-msg hook
+ln -s .git-hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
 
 echo "[setup] Hook installed to .git/hooks/prepare-commit-msg"
